1865101631450710f9862c33194f6b3c9f031954
Annie Cook <annie@nylas.com>
2018-07-13 11:27:48 -0700

    Part 4: Add text parsing functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import re
 
 app = Flask(__name__)
 
@@ -12,7 +13,18 @@ def homepage():
 
 @app.route('/scheduleme', methods=['POST'])
 def scheduleme():
-    return 'I would like to schedule that, but I haven\'t quite figured out how yet...'
+    # Get the text from the request sent by Slack
+    raw_text = request.form.get('text')
+
+    # The raw_text is in wrapped in single quotes in the format '"title" "start" "end"'
+    # This regular expression unwraps the single quotes and puts all strings into an array with
+    # the format: ["title", "start", "end"]
+    text_array = re.findall(r'"(.*?)"', raw_text)
+    if len(text_array) != 3:
+        return 'The format is /scheduleme "[title]" "[start date & time]" "[end date & time]"'
+
+    title, start, end = text_array
+    return f'Sweet I parsed the title: {title}, start: {start} and end: {end}'
 
 
 if __name__ == '__main__':
